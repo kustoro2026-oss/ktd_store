a6f10a0a986f498e02129c489d18d1abedea4569
kustoro2026-oss <kustoroterbatas@gmail.com>
2026-09-13 14:44:50 +0700

    cekkk h

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,12 @@ const nextConfig: NextConfig = {
     formats: ["image/webp", "image/avif"],
     deviceSizes: [640, 768, 1024, 1280, 1536],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "anekadropship.id",
+      },
+    ],
   },
 
   // Enable gzip/brotli compression
